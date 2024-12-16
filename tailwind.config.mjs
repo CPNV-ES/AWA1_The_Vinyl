4237393e7b978592997734bfa17e095f509cf861
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-16 16:06:00 +0100

    refactor: adapt the Login with tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        pacifico: ['"Pacifico"'],
+      },
+    },
+  },
+  plugins: [],
+}
