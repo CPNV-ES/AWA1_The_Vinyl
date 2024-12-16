47d1fbf318e362ac0ac00d13403c78d5ba414403
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-16 11:18:40 +0100

    feat: add the / route to redirect on home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
 import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
+  { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
 ]
 
