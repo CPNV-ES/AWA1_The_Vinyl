69fc080a41639b07edcc539233cd60f990764557
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-16 13:39:14 +0100

    feat: add a navbar on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,43 @@
 <template>
-    <h1>Home</h1>
+    <nav>
+        <button name="logout" class="button-spotify"><b>Logout</b></button>
+    </nav>
+    <div id="main">
+        <section class="screen-split side-player">A</section>
+        <section class="screen-split side-research">B</section>
+    </div>
 </template>
 
+<style>
+    body nav {
+        justify-content: flex-end;
+    }
+
+    #main {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+
+    .screen-split {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 50dvw;
+    }
+
+    .button-spotify {
+        width: 5rem;
+        height: 2rem;
+    }
+
+    @media (min-width: 1200px) {
+        body #main {
+            flex-direction: row;
+        }
+    }
+</style>
 
 <script>
 export default { name: 'Home' };
