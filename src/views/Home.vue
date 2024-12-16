2a1889874ecf33669fa22c929f7e7571f2498153
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-12-16 16:18:15 +0100

    refactor: adapt the Home page with tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,12 @@
 <template>
-    <nav>
-        <button name="logout" class="button-spotify"><b>Logout</b></button>
+    <nav class="justify-end">
+        <button name="logout" class="button-spotify px-3 py-1"><b>Logout</b></button>
     </nav>
-    <div id="main">
-        <section class="screen-split side-player">A</section>
-        <section class="screen-split side-research">B</section>
+    <div class="grow flex flex-col lg:flex-row">
+        <section class="flex justify-center items-center h-full w-[50dvw]">A</section>
+        <section class="flex justify-center items-center h-full w-[50dvw]">B</section>
     </div>
 </template>
-
-<style>
-    body nav {
-        justify-content: flex-end;
-    }
-
-    #main {
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-    }
-
-    .screen-split {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        width: 50dvw;
-    }
-
-    .button-spotify {
-        width: 5rem;
-        height: 2rem;
-    }
-
-    @media (min-width: 1200px) {
-        body #main {
-            flex-direction: row;
-        }
-    }
-</style>
 
 <script>
 export default { name: 'Home' };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <h1 class="align-middle mt-0 text-[10dvh] font-pacifico">The Vinyl</h1>
-    <button name="login" class="button-spotify w-44 h-12">Login to Spotify</button>
+    <button name="login" class="button-spotify px-10 py-3">Login to Spotify</button>
 </template>
 
 <script>
