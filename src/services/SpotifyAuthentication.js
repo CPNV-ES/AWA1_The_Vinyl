f1cd1ca34f672bd48ab1e803f54d6f05a06b9304
Sebastien-Moraz <sebastien@moraz.net>
2024-12-20 08:50:18 +0100

    refactor: remake autentication in SpotifyAnthentication

--- src/services/SpotifyAuthentication.js
+++ src/services/SpotifyAuthentication.js
@@ -1,47 +1,42 @@
-import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+import {AuthorizationCodeWithPKCEStrategy, SpotifyApi} from '@spotify/web-api-ts-sdk';
 
 class SpotifyAuthentication {
 	constructor() {
-		this.accessToken = localStorage.getItem('spotify_access_token') || null;
-	}
-
-	isLoggedIn() {
-		return this.accessToken !== null;
-	}
-
-	async login() {
-		await SpotifyApi.performUserAuthorization(
+		this.implicitGrantStrategy = new AuthorizationCodeWithPKCEStrategy(
 			import.meta.env.VITE_SPOTIFY_CLIENT_ID,
 			import.meta.env.VITE_REDIRECT_TARGET,
-			['user-read-private', 'user-read-email', 'user-library-read', 'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public', 'playlist-modify-private', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-recently-played'],
-			(body) => {
-				localStorage.setItem('spotify_access_token', JSON.stringify(body));
-			}
-		);
-		this.accessToken = JSON.parse(localStorage.getItem('spotify_access_token'));
-		await this.connect();
-	}
-
-	async connect () {
-		const token = JSON.parse(localStorage.getItem('spotify_access_token'));
-		this.sdk = SpotifyApi.withAccessToken(
-			import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-			token
-		);
-	}
-
-	getAccessToken() {
-		if (this.accessToken == null) {
-			throw new Error('No access token found');
+			['user-read-private', 'user-read-email']
+		)
+		this.sdk = new SpotifyApi(this.implicitGrantStrategy);
+		if (localStorage.getItem('isLogin')) {
+			this.isLogin = Boolean(Number(localStorage.getItem('isLogin')))
 		} else {
-			return this.accessToken;
+			this.isLogin = false
+			localStorage.setItem('isLogin', String(0))
 		}
 	}
 
-	logout() {
-		if (this.sdk) this.sdk.logOut();
-		localStorage.removeItem('spotify_access_token');
-		this.accessToken = null;
+	async authenticate() {
+		await this.sdk.authenticate();
+		await localStorage.setItem('isLogin', String(1));
+		this.isLogin = true;
+		window.location.reload();
+	}
+
+	async logout() {
+		await this.implicitGrantStrategy.removeAccessToken()
+		await localStorage.setItem('isLogin', String(0))
+		this.isLogin = false;
+		console.log('logout', this.isLogin)
+		window.location.reload();
+	}
+
+	isLogged() {
+		return this.isLogin
+	}
+
+	getSdk() {
+		return this.sdk
 	}
 }
 
