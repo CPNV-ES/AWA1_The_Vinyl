4f9e05b944ff12ba72a12ecf409797b94c41df55
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-23 23:40:03 +0100

    feat: add the player in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
+<script setup>
+import VinylPlayer from '../components/graphicComponents/VinylPlayer.vue';
+</script>
+
 <template>
     <nav class="justify-end">
         <button name="logout" class="button-spotify px-3 py-1"><b>Logout</b></button>
     </nav>
     <div class="grow flex flex-col lg:flex-row">
-        <section class="flex justify-center items-center h-full w-[50dvw]">A</section>
+        <section class="flex justify-center items-center h-full w-[50dvw]">
+            <VinylPlayer />
+        </section>
         <section class="flex justify-center items-center h-full w-[50dvw]">B</section>
     </div>
 </template>
