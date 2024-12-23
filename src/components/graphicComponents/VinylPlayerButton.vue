9578cbbac229a55b3fdc3a69a3c822e5ecde4e82
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-23 22:55:17 +0100

    feat: add button component to interact with the player

---
+++ src/components/graphicComponents/VinylPlayerButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="transform perspective-1000">
+        <div :class="['bg-neutral-950 text-white', pressed ? 'h-2' : 'h-6', 'origin-bottom transform rotate-x-30']"></div>
+        <div class="bg-black text-white h-fit p-2">{{ content }}</div>
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'VinylPlayerButton',
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    pressed: {
+      type: Boolean,
+      default: false
+    }
+  },
+};
+</script>
