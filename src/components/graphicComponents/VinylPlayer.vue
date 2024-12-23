1f59cba5db9838db03ca417f91113714a6506fd4
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-24 00:14:22 +0100

    fix: update the player buttons interactions and logos

--- src/components/graphicComponents/VinylPlayer.vue
+++ src/components/graphicComponents/VinylPlayer.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue';
 import VinylPlayerButton from './VinylPlayerButton.vue';
 
+import { ForwardIcon, BackwardIcon, PlayIcon, PauseIcon } from '@heroicons/vue/24/solid';
+
 const pressedBackward = ref(false);
 const pressedForward = ref(false);
 const clickedPause = ref(true);
@@ -11,10 +13,18 @@ const clickedPause = ref(true);
     <div id="player" class="perspective-1000">
         <div id="player-top" class="bg-orange-900 h-[50dvh] w-[50dvh]"></div>
         <div id="player-face" class="bg-orange-950 h-[10dvh] w-[50dvh] flex justify-evenly items-center origin-top transform -rotate-x-30">
-            <VinylPlayerButton content="backward" :pressed="pressedBackward" @onPress:pressed="pressedBackward = $event" />
-            <VinylPlayerButton content="pause" :pressed="clickedPause" @click="clickedPause = clickedPause? clickedPause: !clickedPause" />
-            <VinylPlayerButton content="play" :pressed="!clickedPause" @click="clickedPause = !clickedPause? clickedPause: !clickedPause" />
-            <VinylPlayerButton content="forward" :pressed="pressedForward" @onPress:pressed="pressedForward = $event" />
+            <VinylPlayerButton content="backward" :pressed="pressedBackward" @onPress:pressed="pressedBackward = $event">
+                <BackwardIcon class="h-6 w-6 text-neutral-300" />
+            </VinylPlayerButton>
+            <VinylPlayerButton content="pause" :pressed="clickedPause" @click="clickedPause = clickedPause? clickedPause: !clickedPause">
+                <PauseIcon class="h-6 w-6 text-neutral-300" />
+            </VinylPlayerButton>
+            <VinylPlayerButton content="play" :pressed="!clickedPause" @click="clickedPause = !clickedPause? clickedPause: !clickedPause">
+                <PlayIcon class="h-6 w-6 text-neutral-300" />
+            </VinylPlayerButton>
+            <VinylPlayerButton content="forward" :pressed="pressedForward" @onPress:pressed="pressedForward = $event">
+                <ForwardIcon class="h-6 w-6 text-neutral-300" />
+            </VinylPlayerButton>
         </div>
     </div>
 </template>
--- src/components/graphicComponents/VinylPlayerButton.vue
+++ src/components/graphicComponents/VinylPlayerButton.vue
@@ -1,7 +1,9 @@
 <template>
-    <button class="perspective-400" @mousedown="$emit('onPress:pressed', true)" @mouseup="$emit('onPress:pressed', false)">
+    <button class="perspective-200" @mousedown="$emit('onPress:pressed', true)" @mouseup="$emit('onPress:pressed', false)" @mouseleave="$emit('onPress:pressed', false)">
         <div :class="['bg-neutral-950 text-white', pressed ? 'h-2 -mt-3' : 'h-6', 'origin-bottom transform rotate-x-30']"></div>
-        <div class="bg-black text-white h-fit p-2">{{ content }}</div>
+        <div class="bg-black text-white h-fit p-2">
+          <slot></slot>
+        </div>
     </button>
 </template>
 
@@ -9,10 +11,6 @@
 export default {
   name: 'VinylPlayerButton',
   props: {
-    content: {
-      type: String,
-      required: true
-    },
     pressed: {
       type: Boolean,
       default: false
