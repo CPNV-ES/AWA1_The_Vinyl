30620da19e55b67e284ea918c539c30be1a8313d
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-25 15:59:06 +0100

    feat: link the animation of the vinyl to the player's buttons

--- src/components/graphicComponents/Vinyl.vue
+++ src/components/graphicComponents/Vinyl.vue
@@ -1,10 +1,18 @@
 <template>
-    <div class="relative bg-vinyl-texture w-5/6 h-5/6 rounded-full flex justify-center items-center shadow-md">
-        <div class="absolute h-full w-full rounded-full bg-grainy-texture opacity-100 mix-blend-difference animate-spin-slow"></div>
-        <img src="/spotify-icon.svg" alt="" class="bg-neutral-800 w-1/3 h-1/3 rounded-full animate-spin-slow" />
-    </div>
+  <div class="relative bg-vinyl-texture w-5/6 h-5/6 rounded-full flex justify-center items-center shadow-md">
+    <div :class="['absolute h-full w-full rounded-full bg-grainy-texture opacity-100 mix-blend-difference animate-spin-slow', isPlaying? '[animation-play-state:running]' : '[animation-play-state:paused]']"></div>
+    <img src="/spotify-icon.svg" alt="" :class="['bg-neutral-800 w-1/3 h-1/3 rounded-full animate-spin-slow', isPlaying? '[animation-play-state:running]' : '[animation-play-state:paused]']" />
+  </div>
 </template>
 
 <script>
-export default {name: 'Vinyl'};
+export default {
+  name: 'Vinyl',
+  props: {
+    isPlaying: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
 </script>
--- src/components/graphicComponents/VinylPlayer.vue
+++ src/components/graphicComponents/VinylPlayer.vue
@@ -6,22 +6,22 @@ import Vinyl from './Vinyl.vue';
 
 const pressedBackward = ref(false);
 const pressedForward = ref(false);
-const clickedPause = ref(true);
+const isPlaying = ref(false);
 </script>
 
 <template>
     <div id="player" class="perspective-1000">
         <div id="player-top" class="bg-orange-900 h-[50dvh] w-[50dvh] flex justify-center items-center origin-bottom transform rotate-x-60 overflow-hidden">
-            <Vinyl />
+            <Vinyl :isPlaying/>
         </div>
         <div id="player-face" class="bg-orange-950 h-[10dvh] w-[50dvh] flex justify-evenly items-center origin-top transform -rotate-x-30">
             <VinylPlayerButton content="backward" :pressed="pressedBackward" @onPress:pressed="pressedBackward = $event">
                 <BackwardIcon class="h-6 w-6 text-neutral-300" />
             </VinylPlayerButton>
-            <VinylPlayerButton content="pause" :pressed="clickedPause" @click="clickedPause = clickedPause? clickedPause: !clickedPause">
+            <VinylPlayerButton content="pause" :pressed="!isPlaying" @click="isPlaying = !isPlaying? isPlaying: !isPlaying">
                 <PauseIcon class="h-6 w-6 text-neutral-300" />
             </VinylPlayerButton>
-            <VinylPlayerButton content="play" :pressed="!clickedPause" @click="clickedPause = !clickedPause? clickedPause: !clickedPause">
+            <VinylPlayerButton content="play" :pressed="isPlaying" @click="isPlaying = isPlaying? isPlaying: !isPlaying">
                 <PlayIcon class="h-6 w-6 text-neutral-300" />
             </VinylPlayerButton>
             <VinylPlayerButton content="forward" :pressed="pressedForward" @onPress:pressed="pressedForward = $event">
