6940032c980350b2577039f8b1204c2638a225cb
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-25 17:30:48 +0100

    feat: add the view of the search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid';
+</script>
+<template>
+    <div class="flex-grow flex items-center relative">
+        <input
+            class="align-middle rounded-full border-neutral-500 border bg-white outline-none cursor-pointer transition-all duration-300 h-full w-full px-3 py-1"
+            type="text" placeholder="Search for a song" />
+        <MagnifyingGlassIcon class="h-6 w-6 text-neutral-500 absolute right-2" />
+    </div>
+</template>
+
+
+<script>
+export default { name: 'SearchBar' };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
 import VinylPlayer from '../components/graphicComponents/VinylPlayer.vue';
+import SearchBar from '../components/SearchBar.vue';
 </script>
 
 <template>
-    <nav class="justify-end">
+    <nav class="justify-between flex gap-4">
+        <SearchBar />
         <button name="logout" class="button-spotify px-3 py-1"><b>Logout</b></button>
     </nav>
     <div class="grow flex flex-col lg:flex-row">
