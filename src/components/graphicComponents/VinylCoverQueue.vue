cfdd9412ccd74b37a3752c8ebaa1e3cc2575beb9
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-27 14:52:34 +0100

    feat: add the queue graphic component

---
+++ src/components/graphicComponents/VinylCoverQueue.vue
@@ -0,0 +1,21 @@
+<script setup>
+import VinylCover from './VinylCover.vue';
+</script>
+
+<template>
+    <div class="flex justify-center items-center h-full w-[50dvw] perspective-1000 gap-1">
+        <VinylCover v-for="(song, index) in queue" :key="song.id" :song="song" :zIndex="index > queue.length/2? -index : index" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VinylCoverQueue',
+    props: {
+        queue: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
