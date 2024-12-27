757eedbe63ae3c73dde7dabeba31add5a7dc2b7d
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-27 17:17:31 +0100

    refactor: move the hover detection of the cover to separate business and view logic

--- src/components/graphicComponents/VinylCoverQueue.vue
+++ src/components/graphicComponents/VinylCoverQueue.vue
@@ -10,17 +10,17 @@ const preview = ref({});
     <div class="flex flex-col justify-center h-full">
         <div class="h-[30dvh] w-[30dvh] relative">
             <img v-if="preview.value" :src="preview.value.cover" alt="" class="h-full w-full" />
-            <div class="h-full w-full absolute -z-50 top-0 -translate-x-1/2 flex justify-center items-center">
-                <Vinyl />
+            <div v-if="preview.value" class="h-full w-full absolute -z-50 top-0 -translate-x-1/2 flex justify-center items-center">
+                <Vinyl :cover="preview.value.cover" />
             </div>
         </div>
         <p class="flex flex-col items-start ">
-            <span class="font-bold">{{ preview.value.title }}</span>
-            <span class="font-semibold text-neutral-700 text-xs">{{ preview.value.artist }}</span>
+            <span v-if="preview.value" class="font-bold">{{ preview.value.title }}</span>
+            <span v-if="preview.value" class="font-semibold text-neutral-700 text-xs">{{ preview.value.artist }}</span>
         </p>
     </div>
     <div class="flex flex-grow justify-center items-center h-full perspective-1000 gap-1">
-        <VinylCover @onHover:setPreview="preview.value = $event" v-for="(song, index) in queue" :key="song.id" :song="song" :zIndex="index > queue.length / 2 ? -index : index" />
+        <VinylCover @mouseover="preview.value = song" v-for="(song, index) in queue" :key="song.id" :song="song" :zIndex="index > queue.length / 2 ? -index : index" :isPreviewed="preview.value === song" />
     </div>
 </template>
 
