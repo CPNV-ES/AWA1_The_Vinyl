e007bbb588f0abc5da9f8907260f24c6c2903a33
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-28 20:43:21 +0100

    feat: add a third face on the vinyl player

--- src/components/graphicComponents/VinylPlayer.vue
+++ src/components/graphicComponents/VinylPlayer.vue
@@ -9,11 +9,12 @@ const pressedForward = ref(false);
 </script>
 
 <template>
-    <div id="player" class="">
-        <div id="player-top" class="transform-style-flat bg-orange-900 h-[50dvh] w-[50dvh] flex justify-center items-center origin-bottom transform rotate-x-60 overflow-hidden">
+    <div id="player" class="relative">
+        <div id="player-top" class="bg-orange-900 h-[50dvh] w-[50dvh] flex justify-center items-center origin-bottom transform rotate-x-60 overflow-hidden">
             <Vinyl v-if="player.currentSong" :isPlaying="player.isPlaying" :cover="player.currentSong.cover" />
         </div>
-        <div id="player-face" class="transform-style-flat bg-orange-950 h-[10dvh] w-[50dvh] flex justify-evenly items-center origin-top transform -rotate-x-30">
+        <div id="player-right" class="absolute h-[50dvh] w-[10dvh] bg-orange-950 brightness-75 top-0 -right-[10dvh] origin-bottom-left transform rotate-x-60 rotate-y-90"></div>
+        <div id="player-face" class="bg-orange-950 h-[10dvh] w-[50dvh] flex justify-evenly items-center origin-top transform -rotate-x-30">
             <VinylPlayerButton :pressed="pressedBackward" @onPress:pressed="pressedBackward = $event" @click="player.previous()">
                 <BackwardIcon class="h-[3dvh] w-[3dvh] text-neutral-300" />
             </VinylPlayerButton>
