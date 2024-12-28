6c34353dddd5ff490ab936d536dcac59746bc264
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-12-28 18:07:50 +0100

    feat: limit the amount of shown covers in the queue

--- src/components/graphicComponents/VinylCoverQueue.vue
+++ src/components/graphicComponents/VinylCoverQueue.vue
@@ -1,16 +1,15 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import VinylCover from './VinylCover.vue';
 import Vinyl from './Vinyl.vue';
-
-const preview = ref({});
 </script>
 
 <template>
     <div class="flex flex-col justify-center h-full">
         <div class="h-[30dvh] w-[30dvh] relative">
             <img v-if="preview.value" :src="preview.value.cover" alt="" class="h-full w-full" />
-            <div v-if="preview.value" class="h-full w-full absolute -z-50 top-0 -translate-x-1/2 flex justify-center items-center">
+            <div v-if="preview.value"
+                class="h-full w-full absolute -z-50 top-0 -translate-x-1/2 flex justify-center items-center">
                 <Vinyl :cover="preview.value.cover" />
             </div>
         </div>
@@ -19,8 +18,12 @@ const preview = ref({});
             <span v-if="preview.value" class="font-semibold text-neutral-700 text-xs">{{ preview.value.artist }}</span>
         </p>
     </div>
-    <div class="flex flex-grow justify-center items-center h-full perspective-1000 gap-1">
-        <VinylCover @mouseover="preview.value = song" v-for="(song, index) in queue" :key="song.id" :song="song" :zIndex="index > queue.length / 2 ? -index : index" :isPreviewed="preview.value === song" />
+    <div class="flex h-full justify-center flex-grow perspective-1000">
+        <div class="flex w-full items-center h-full gap-1 mx-[5dvh]">
+            <VinylCover @mouseover="preview.value = song" v-for="(song, index) in visibleCovers" :key="song.id"
+                :zIndex="index > visibleCoverCount / 2 ? -index : index" :song="song" :isPreviewed="preview.value === song"
+                :translucent="visibleCoverStartIndex > 0 && index == 0 || index == visibleCoverCount -1 && visibleCoverStartIndex < queue.length - visibleCoverCount" />
+        </div>
     </div>
 </template>
 
@@ -32,6 +35,16 @@ export default {
             type: Array,
             required: true
         }
+    },
+    data() {
+        return {
+            preview: ref({}),
+            visibleCoverCount: 10,
+            visibleCoverStartIndex: ref(0),
+            visibleCovers: computed(() => {
+                return this.queue.slice(this.visibleCoverStartIndex, this.visibleCoverStartIndex + this.visibleCoverCount);
+            })
+        };
     }
 };
 </script>
