23f39557b18389eff2ed78c8c452689cfbee13ef
Benjamin Fontana <ben192003@gmail.com>
2025-01-06 13:36:11 +0100

    fix: wrong import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import router from './router'
-import './assets/styles.css'
+import './assets/tailwind.css'
 import App from './App.vue'
 import { gsap } from "gsap";
 
