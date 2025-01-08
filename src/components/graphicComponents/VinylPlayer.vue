255d25af92b1e81b0c9b4f46f88a564a8f36e727
Benjamin Fontana <ben192003@gmail.com>
2025-01-08 16:18:03 +0100

    fix: buttons effect on playing

--- src/components/graphicComponents/VinylPlayer.vue
+++ src/components/graphicComponents/VinylPlayer.vue
@@ -11,17 +11,19 @@ const pressedForward = ref(false);
 <template>
     <div id="player" class="relative">
         <div id="player-top" class="bg-orange-900 h-[50dvh] w-[50dvh] flex justify-center items-center origin-bottom transform rotate-x-60 overflow-hidden">
-            <Vinyl v-if="store.current_track" :isPlaying="!store.is_paused" :cover="store.current_track.album.images[0].url" />
+            <template v-if="store.is_active">
+                <Vinyl v-if="store.current_track" :isPlaying="!store.is_paused" :cover="store.current_track.album.images[0].url" />
+            </template>
         </div>
         <div id="player-right" class="absolute h-[50dvh] w-[10dvh] bg-orange-950 brightness-75 top-0 -right-[10dvh] origin-bottom-left transform rotate-x-60 rotate-y-90"></div>
         <div id="player-face" class="bg-orange-950 h-[10dvh] w-[50dvh] flex justify-evenly items-center origin-top transform -rotate-x-30">
             <VinylPlayerButton :pressed="pressedBackward" @onPress:pressed="pressedBackward = $event" @click="player.playPreviousTrack()">
                 <BackwardIcon class="h-[3dvh] w-[3dvh] text-neutral-300" />
             </VinylPlayerButton>
-            <VinylPlayerButton :pressed="!player.is_paused" @click="player.togglePlay()">
+            <VinylPlayerButton :pressed="store.is_paused" @click="player.togglePlay()">
                 <PauseIcon class="h-[3dvh] w-[3dvh] text-neutral-300" />
             </VinylPlayerButton>
-            <VinylPlayerButton :pressed="player.is_paused" @click="player.togglePlay()">
+            <VinylPlayerButton :pressed="!store.is_paused" @click="player.togglePlay()">
                 <PlayIcon class="h-[3dvh] w-[3dvh] text-neutral-300" />
             </VinylPlayerButton>
             <VinylPlayerButton :pressed="pressedForward" @onPress:pressed="pressedForward = $event" @click="player.playNextTrack()">
@@ -37,7 +39,6 @@ export default {
     props: {
         player: {
             type: Object,
-            required: true
         },
         store: {
             type: Object,
