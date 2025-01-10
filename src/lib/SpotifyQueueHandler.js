393511f1b03db4c3596b9bce14fc7697bede8bab
Benjamin Fontana <ben192003@gmail.com>
2025-01-10 12:05:36 +0100

    refactor: improve readiness and error handling

--- src/lib/SpotifyQueueHandler.js
+++ src/lib/SpotifyQueueHandler.js
@@ -7,37 +7,55 @@ class SpotifyQueueHandler {
 	}
 
 	async addSongToQueue(song) {
-		if (!this.store.current_track.uri) {
-			await this.sdk.player.startResumePlayback(
-				this.store.device_id,
-				null,
-				[song.uri]
-			);
-		} else {
-			await this.addSongToQueueRequest(
-				await SpotifyAuthentication.getAccessToken(),
-				this.store.device_id,
-				song.uri
-			);
-
-			this.store.addToQueue(song);
+		try {
+			if (!this.store.current_track.uri) {
+				await this._startPlaybackWithSong(song.uri);
+			} else {
+				await this._addSongToQueueRequest(song.uri);
+				this.store.addToQueue(song);
+			}
+		} catch (error) {
+			console.error("Failed to add song to queue", error);
 		}
 	}
 
-	async addSongToQueueRequest(accessTokens, deviceId, songUri) {
-		const response = await fetch(
-			"https://api.spotify.com/v1/me/player/queue?uri=" +
-				songUri +
-				"&device_id=" +
-				deviceId,
-			{
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json,",
-					Authorization: "Bearer " + accessTokens,
-				},
+	async _startPlaybackWithSong(songUri) {
+		try {
+			await this.sdk.player.startResumePlayback(
+				this.store.device_id,
+				null,
+				[songUri]
+			);
+		} catch (error) {
+			console.error("Error starting playback with song:", error);
+			throw error;
+		}
+	}
+
+	async _addSongToQueueRequest(songUri) {
+		const accessToken = await SpotifyAuthentication.getAccessToken();
+
+		try {
+			const response = await fetch(
+				`https://api.spotify.com/v1/me/player/queue?uri=${songUri}&device_id=${this.store.device_id}`,
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${accessToken}`,
+					},
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error(
+					`Failed to add song to queue: ${response.statusText}`
+				);
 			}
-		);
+		} catch (error) {
+			console.error("Error adding song to queue:", error);
+			throw error;
+		}
 	}
 }
 
