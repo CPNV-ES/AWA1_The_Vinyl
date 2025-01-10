bf5c9fd47915e09e0b5407700a1159f4aad2c9a7
SebastienMoraz <sebastien@moraz.net>
2025-01-10 18:35:35 +0100

    fix: remove device log

--- src/lib/SpotifyPlayer.js
+++ src/lib/SpotifyPlayer.js
@@ -97,13 +97,8 @@ class SpotifyPlayer {
 	 */
 	#addPlayerEventListeners() {
 		this.player.addListener("ready", ({ device_id }) => {
-			console.log("Ready with Device ID", device_id);
 			this.store.setDeviceId(device_id);
 			this.#transferPlayback(device_id);
-		});
-
-		this.player.addListener("not_ready", ({ device_id }) => {
-			console.log("Device ID has gone offline", device_id);
 		});
 
 		this.player.addListener("player_state_changed", (state) => {
