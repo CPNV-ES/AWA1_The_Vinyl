515777e11089e3cbeaa938bdc3b3a2e8988aabec
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-01-11 17:07:42 +0100

    feat: add the album cover to the mobile previews

--- src/components/Snap.vue
+++ src/components/Snap.vue
@@ -1,24 +1,23 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import {useSpotifyStore} from "../stores/spotify";
 
 const animation = { duration: 1, ease: "power1.inOut" };
-const queue = [
-  "#FFAA00",
-  "#AA00FF",
-  "#00AAFF",
-  "#FF5500",
-  "#55FF00",
-  "#0055FF",
-]
+
+const store = useSpotifyStore();
+
+// add a new 
+
+
 const currentPreview = ref(0);
 
 const createSlide = (slide) => {
-  const slideElement = document.createElement("div");
-  slideElement.classList.add("h-full", "w-full", "absolute", "brightness-50");
-  slideElement.style.backgroundColor = slide;
-  slideElement.innerHTML = slide;
-  return slideElement;
-}
+  const imgElement = document.createElement("img");
+  imgElement.src = slide.cover;
+  imgElement.alt = "aaa";
+  imgElement.classList.add("h-full", "w-full", "absolute", "brightness-50", "object-cover");
+  return imgElement;
+};
 
 onMounted(() => {
   const stackToSee = document.getElementById("stack-toSee");
@@ -27,20 +26,35 @@ onMounted(() => {
   const btnNextPreview = document.getElementById("btn-next-preview");
   const btnPreviousPreview = document.getElementById("btn-previous-preview");
 
-  for (let i = 0; i < queue.length; i++) {
-    const slideElement = createSlide(queue[i]);
-    slideElement.style.zIndex = queue.length - i;
+  watch(
+    () => store.queue,
+    () => {
+      generatePreviews();
+    },
+    { deep: true }
+  );
 
-    if (i === 0) {
-      stackCurrent.appendChild(slideElement);
-      slideElement.classList.toggle("brightness-50");
-    } else {
-      stackToSee.appendChild(slideElement);
+  const generatePreviews = () => {
+    currentPreview.value = 0;
+    stackToSee.innerHTML = "";
+    stackCurrent.innerHTML = "";
+    stackSeen.innerHTML = "";
+
+    for (let i = 0; i < store.queue.length; i++) {
+      const slideElement = createSlide(store.queue[i]);
+      slideElement.style.zIndex = store.queue.length - i;
+      
+      if (i === 0) {
+        stackCurrent.appendChild(slideElement);
+        slideElement.classList.toggle("brightness-50");
+      } else {
+        stackToSee.appendChild(slideElement);
+      }
     }
-  }
+  };
 
   const moveNext = () => {
-    if (currentPreview.value === queue.length - 1) return;
+    if (currentPreview.value === store.queue.length - 1) return;
     currentPreview.value = currentPreview.value + 1;
     const seen = stackCurrent.firstChild;
     if (seen) {
@@ -88,19 +102,19 @@ onMounted(() => {
 
 
 <template>
-  <section class="carrousel w-screen h-[30dvh] bg-red-400 overflow-hidden flex flex-col">
+  <section class="carrousel w-screen h-[30dvh] overflow-hidden flex flex-col">
     <div class="flex w-full h-5/6 justify-evenly items-center px-[6dvw]">
       <div id="stack-seen" class="w-[17dvw] aspect-square relative -z-30"></div>
       <div id="stack-current" class="w-[20dvw] aspect-square relative"></div>
       <div id="stack-toSee" class="w-[17dvw] aspect-square relative -z-30 transition-colors"></div>
     </div>
     <div class="w-full flex items-center h-1/6">
-      <button id="btn-previous-preview" class="hover:bg-yellow-300 flex-1">previous</button>
-      <p class="flex-1 flex flex-col text-nowrap items-center">
-        <span v-if="currentPreview => 0" class="font-bold text-md w-fit">{{ currentPreview }}</span>
-        <span v-if="currentPreview => 0" class="font-semibold w-fit text-neutral-700 text-sm">queue[currentPreview].artist</span>
+      <button id="btn-previous-preview" class="flex-1"><</button>
+      <p class="w-[60dvw] flex flex-col text-nowrap items-center">
+        <span v-if="store.queue[currentPreview]" class="font-bold text-md w-fit">{{ store.queue[currentPreview].title }}</span>
+        <span v-if="store.queue[currentPreview]" class="font-semibold w-fit text-neutral-700 text-sm">{{store.queue[currentPreview].artist}}</span>
       </p>
-      <button id="btn-next-preview" class="hover:bg-yellow-300 flex-1">next</button>
+      <button id="btn-next-preview" class="flex-1">></button>
     </div>
   </section>
 </template>
